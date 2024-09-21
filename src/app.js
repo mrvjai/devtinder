b9ca4c802890ac9ed7947df2ad9211d3aa3887a9
kittuj <keerthana1669@gmail.com>
2024-09-21 11:59:55 +0530

    dynamic data post to db

--- src/app.js
+++ src/app.js
@@ -3,32 +3,28 @@ const app = express();
 const DB = require('./config/datanase');
 const userModel = require('./models/users')
 
+app.use(express.json())
 
-app.post('/signup',async(req,res)=>{
-    console.log(req.query.body)
-    const userData = new userModel( {
-        firstName:"vijay",
-        lastName:"kumar",
-        surName:"Nallagatla",
-        age:26,
-        gender:"Male"
-    })
 
-await userData.save();
+app.post('/signup', async (req, res) => {
+    console.log(req.body)
+    const userData = new userModel(req.body)
 
-res.send("data saved succesfully")
+    await userData.save();
+
+    res.send("data saved succesfully")
 
 })
 
 
 DB()
-.then(()=>{
-    console.log("connected succesfully");
-    app.listen(7777, () => {
-        console.log("app is listening on port:7777")
+    .then(() => {
+        console.log("connected succesfully");
+        app.listen(7777, () => {
+            console.log("app is listening on port:7777")
+        })
     })
-})
-.catch(()=>{
-    console.log("not connected ......")
-})
+    .catch(() => {
+        console.log("not connected ......")
+    })
 
